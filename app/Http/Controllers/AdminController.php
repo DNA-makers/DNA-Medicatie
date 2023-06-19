416b339db125167fd9152b10e61582d05480430c
Jasper Samson <s1136647@student.hsleiden.nl>
2023-06-19 11:55:56 +0200

    adminpanel edit&delete

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Advise;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Admin;
@@ -16,6 +17,35 @@ class AdminController extends Controller
             return response()->json(['message' => 'Succesvol ingelogd'], 200);
         } else {
             return response()->json(['message' => 'Email en/of wachtwoord is onjuist'], 400);
+        }
+    }
+
+    public function updateAdvice(Request $request) {
+        $data = $request->all();
+
+        foreach ($data as $item) {
+            $id = $item['id'];
+            $title = $item['title'];
+            $advice = $item['advise'];
+
+            $adviceRecord = Advise::findOrFail($id);
+
+            $adviceRecord->title = $title;
+            $adviceRecord->advise = $advice;
+
+            $adviceRecord->save();
+        }
+
+        return response()->json("De data is succesvol aangepast");
+    }
+
+    public function deleteAdvice($id) {
+        try {
+            $advice = Advise::findOrFail($id);
+            $advice->delete();
+            return response()->json(['message' => 'Advice row deleted succesfully']);
+        } catch (\Exception $e) {
+            return response()->json(['message' => 'Failed to delete the advice row']);
         }
     }
 }
